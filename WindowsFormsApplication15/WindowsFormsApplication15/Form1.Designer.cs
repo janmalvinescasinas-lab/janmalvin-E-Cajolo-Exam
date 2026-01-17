d35103c6dc7986cbce335a904a974387fd907331
janmalvinescasinas-lab <janmalvinescasinas@gmail.com>
2026-01-17 11:09:50 +0800

    Add files via upload

---
+++ WindowsFormsApplication15/WindowsFormsApplication15/Form1.Designer.cs
@@ -0,0 +1,142 @@
+﻿namespace WindowsFormsApplication15
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.txtNote = new System.Windows.Forms.TextBox();
+            this.test = new System.Windows.Forms.ListBox();
+            this.bAdd = new System.Windows.Forms.Button();
+            this.cRemove = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(61, 33);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(80, 13);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Enter Yor Note:";
+            this.label1.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(329, 30);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(63, 13);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Your Notes:";
+            // 
+            // txtNote
+            // 
+            this.txtNote.Location = new System.Drawing.Point(147, 30);
+            this.txtNote.Name = "txtNote";
+            this.txtNote.Size = new System.Drawing.Size(100, 20);
+            this.txtNote.TabIndex = 3;
+            this.txtNote.TextChanged += new System.EventHandler(this.textBox1_TextChanged);
+            // 
+            // test
+            // 
+            this.test.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(255)))), ((int)(((byte)(192)))));
+            this.test.FormattingEnabled = true;
+            this.test.Location = new System.Drawing.Point(332, 46);
+            this.test.Name = "test";
+            this.test.Size = new System.Drawing.Size(318, 108);
+            this.test.TabIndex = 4;
+            // 
+            // bAdd
+            // 
+            this.bAdd.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(192)))), ((int)(((byte)(0)))));
+            this.bAdd.Location = new System.Drawing.Point(147, 78);
+            this.bAdd.Name = "bAdd";
+            this.bAdd.Size = new System.Drawing.Size(75, 39);
+            this.bAdd.TabIndex = 5;
+            this.bAdd.Text = "Add";
+            this.bAdd.UseVisualStyleBackColor = false;
+            this.bAdd.Click += new System.EventHandler(this.bAdd_Click);
+            // 
+            // cRemove
+            // 
+            this.cRemove.BackColor = System.Drawing.Color.Tomato;
+            this.cRemove.Location = new System.Drawing.Point(147, 123);
+            this.cRemove.Name = "cRemove";
+            this.cRemove.Size = new System.Drawing.Size(75, 40);
+            this.cRemove.TabIndex = 6;
+            this.cRemove.Text = "Remove";
+            this.cRemove.UseVisualStyleBackColor = false;
+            this.cRemove.Click += new System.EventHandler(this.cRemove_Click);
+            // 
+            // button3
+            // 
+            this.button3.BackColor = System.Drawing.Color.Red;
+            this.button3.Location = new System.Drawing.Point(147, 169);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(75, 40);
+            this.button3.TabIndex = 7;
+            this.button3.Text = "Clear";
+            this.button3.UseVisualStyleBackColor = false;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.Violet;
+            this.ClientSize = new System.Drawing.Size(740, 262);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.cRemove);
+            this.Controls.Add(this.bAdd);
+            this.Controls.Add(this.test);
+            this.Controls.Add(this.txtNote);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.WindowsDefaultBounds;
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox txtNote;
+        private System.Windows.Forms.ListBox test;
+        private System.Windows.Forms.Button bAdd;
+        private System.Windows.Forms.Button cRemove;
+        private System.Windows.Forms.Button button3;
+    }
+}
+
